153c66ced7bda87ef47803276e4ddf72bf1da326
user.name <cybertrooper1971@gmail.com>
2018-02-17 20:08:57 -0700

    first commit of backend code

---
+++ blogApiRouter.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router =  express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const {BlogPosts} = require('./models');
+
+function blogPost() {
+    return 'Hey there, this is a blog post about learning how to ' +
+    'write server-side javascript. The cool thing about it it, you can' +
+    'create and manipulate databases of all kinds. Create Read Update Delete' +
+    'are the methods we use to do that. There is a lot more involved' +
+    'than just that, though. Let\'s proceed through the curriculum and' +
+    'to learn more. '
+}
+
+BlogPosts.create(
+    'A Deeper Understanding', blogPost(), 'A. Noob'
+);
+BlogPosts.create(
+    'A Greater Understanding', blogPost(), 'A. N. Imposter'
+);
+
+router.get('/', (req, res) => {
+    res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length; i += 1) {
+        const field = requiredFields[i];
+            if(!(field in req.body)) {
+                const message = `Missing \`${field}\` in request body`
+                console.error(message);
+                return res.status(400).send(message);
+            }
+    }
+const item = BlogPosts.create(
+    req.body.title, req.body.content, req.body.author);
+res.status(201).json(item);
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+    const requiredFields = [
+        'id', 'title', 'content', 'author']
+        for (let i = 0; i < requiredFields.length; i += 1) {
+            const field = requiredFields[i];
+            if (!(field in req.body)) {
+                const message = `Missing \`${field}\` in request body`
+                console.error(message);
+                return res.status(400).send(message);
+            }
+        }
+if (req.params.id !== req.body.id) {
+    const message = (
+        `Request path id (${req.params.id}) and request body id`
+        `(${req.body.id}) must match`);
+        console.error(message);
+        return res.status(400).send(message);
+    
+}
+    console.log(`Updating blog post with id \`${req.params.id}\``);
+    const updatedItem = BlogPosts.update({
+        id: req.params.id,
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author
+    });
+    
+});
+
+router.delete('/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog post with id \`${req.params.ID}\``);
+    res.status(204).end();
+});
+
+module.exports = router;
