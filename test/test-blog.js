c1dcef7f6d8e0c9b8771e8613709c0992951bfa9
user.name <cybertrooper1971@gmail.com>
2018-02-26 12:51:02 -0700

    typos fixed in test-blog file

--- test/test-blog.js
+++ test/test-blog.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
-const chaiHttp = require ('chai-http');
+const chaiHttp = require('chai-http');
 const {app, closeServer, runServer} = require('../server');
 const expect = chai.expect;
-chai.use(chai.Http);
+chai.use(chaiHttp);
 
 describe('blogPosts', function() {
     before(function() {
