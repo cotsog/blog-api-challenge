056151d9798184c7556149572242b214aca74c4c
user.name <cybertrooper1971@gmail.com>
2018-02-25 18:28:52 -0700

    isEqual passing

--- mocha-chai-demo/test/test-is-equal.js
+++ mocha-chai-demo/test/test-is-equal.js
@@ -1,7 +1,26 @@
 const isEqual = require('../isEqual');
 const expect = require('chai').expect;
 describe('isEqual', function() {
-    it('should give right answers for equal and unequal inputs', function() {
-        expect(isEqual(1, 1)).to.be.true;
+    it('should give right answers for both equal and unequal inputs', function() {
+        const equalInputs = [
+            [1, 1],
+            [true, true],
+            ['foo', 'foo']
+        ];
+        equalInputs.forEach(function(input) {
+            const answer = isEqual(input[0], input[1]);
+            expect(answer).to.be.true;
+        });
+        const unequalInputs = [
+            ['1', 1],
+            ['true', false],
+            [1, 2],
+            [1, true],
+            [0, false]
+        ];
+        unequalInputs.forEach(function(input) {
+            const answer = isEqual(input[0], input[1]);
+            expect(answer).to.be.false;
+        });
     });
 });
